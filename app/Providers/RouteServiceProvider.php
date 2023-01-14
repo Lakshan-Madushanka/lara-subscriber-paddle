a70a95fc2915a0be50ac082d8d28e89b86d9fca9
Lakshan Madushanka <epmadushanka@gmail.com>
2023-01-14 16:52:04 +0530

    install and setup jetstream and laravel-ide-helper

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -46,7 +46,6 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            // @phpstan-ignore-next-line
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
     }
