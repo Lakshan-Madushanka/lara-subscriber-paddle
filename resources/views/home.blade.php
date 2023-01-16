07f916d5820b08ab9f57d417da1dc15b6d8973df
Lakshan Madushanka <epmadushanka@gmail.com>
2023-01-16 10:31:42 +0530

    Create home blade.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+Cr<x-guest-layout>
+    <div class="min-h-screen flex justify-center items-center w-full ">
+        <div class="flex justify-center w-full">
+            <div class="rounded-lg shadow-2xl bg-white w-1/3">
+                <p class="text-6xl bg-cyan-500 shadow-lg shadow-cyan-500/50 text-white text-center p-10">
+                    SUBSCRIBE
+                </p>
+                <div class="p-6">
+                    <p class="text-gray-700 text-base mb-4 p-8 text-2xl">
+                       Subscribe one of out plans to continue.
+                    </p>
+                    <button type="button"
+                            class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                        Show plans
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
