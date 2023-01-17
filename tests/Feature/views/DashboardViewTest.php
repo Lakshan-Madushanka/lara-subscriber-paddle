4e4282e4450cc6315a2cc693091e079135eb8e30
Lakshan Madushanka <epmadushanka@gmail.com>
2023-01-17 10:58:11 +0530

    Test home dashboard view.

---
+++ tests/Feature/views/DashboardViewTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
+use function Pest\Laravel\get;
+
+it('redirect to product page if not subscribed', function () {
+    Sanctum::actingAs(User::factory()->create());
+
+    $response = get('/dashboard');
+
+    $response->assertRedirect();
+    $response->assertSee('home');
+});
+
